18c62ef07d520ecc4feabfffe22a41022f445a4e
neozvbambah <neozvbambah@gmail.com>
2025-05-01 03:47:17 +0530

    registration shifted to tally

--- src/app/waitlist.tsx
+++ src/app/waitlist.tsx
@@ -3,7 +3,7 @@ import { useRouter } from 'next/navigation'
 const Waitlist = () => {
 const router = useRouter()
   return (
-    <button className="bg-gray-600 hover:bg-gray-500 text-amber-50 font-bold cursor-pointer p-4 rounded-sm" onClick={()=>{router.push('https://form.typeform.com/to/yT8hl93D')}}>Join Waitlist</button>
+    <button className="bg-gray-600 hover:bg-gray-500 text-amber-50 font-bold cursor-pointer p-4 rounded-sm" onClick={()=>{router.push('https://tally.so/r/3NE1kW')}}>Join Waitlist</button>
   )
 }
 export default Waitlist;
